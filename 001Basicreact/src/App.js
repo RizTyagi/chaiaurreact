c064e2c8bdcc3e48647c342406512a142dd8ac85
rizwan.tyagi <rizwan.tyagi@nic.in>
2023-12-19 10:36:03 +0530

    03

---
+++ 001Basicreact/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Chai from './chai'
+function App() {
+  return (
+    <>
+    <Chai />
+  <h1>React new demo</h1>
+  <p>render dom</p>
+  </>
+  )
+}
+export default App;
---
+++ 001Basicreact/src/chai.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function Chai(){
+    return(
+        <h3>Chai is ready !!!</h3>
+    )
+}
